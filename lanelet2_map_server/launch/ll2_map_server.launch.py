d5131a77bb268d7276cffbaefb1f534787693640
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2023-07-11 10:15:43 +0200

    add use_sim_time as launch-argument to launch-file

--- lanelet2_map_server/launch/ll2_map_server.launch.py
+++ lanelet2_map_server/launch/ll2_map_server.launch.py
@@ -8,10 +8,16 @@ from launch.substitutions import LaunchConfiguration, FindExecutable
 
 def generate_launch_description():
     node_name = LaunchConfiguration('node_name')
+    use_sim_time_arg = LaunchConfiguration('use_sim_time_arg')
 
     node_name_launch_arg = DeclareLaunchArgument(
         'node_name',
         default_value='ll2_map_server'
+    )
+
+    use_sim_time_launch_arg = DeclareLaunchArgument(
+        'use_sim_time_arg',
+        default_value='False'
     )
 
     ll2_map_server_node = Node(
@@ -19,13 +25,22 @@ def generate_launch_description():
             executable='lanelet2_map_server',
             name=node_name
     )
+
+    sim_time_param = SetParameter(name='use_sim_time',
+            value=LaunchConfiguration('use_sim_time_arg'),
+            condition=LaunchConfigurationNotEquals('use_sim_time_arg', "None")
+    )
     
     return LaunchDescription([
         node_name_launch_arg,
+        use_sim_time_launch_arg,
         ll2_map_server_node,
         RegisterEventHandler(
             OnProcessStart(
-                target_action=ll2_map_server_node
+                target_action=ll2_map_server_node,
+                on_start=[
+                    sim_time_param
+                ]
             )
         )
     ])
