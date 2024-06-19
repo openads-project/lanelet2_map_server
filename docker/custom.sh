12f2720f1ff17ab8a5096eb64d93f7f79007b41a
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-06-19 14:30:35 +0200

    rename folder to default

--- docker/custom.sh
+++ docker/custom.sh
@@ -3,6 +3,6 @@ mkdir -p /data/maps/carla
 ln -s /docker-ros/additional-files /data/maps
 
 # add symbolic links to provide default maps for the carla_its_adapter
-ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/ATC_demo_2024-05-24.osm /data/maps/carla/ATC.osm
-ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/carla/CampusVaalsHalifax.osm
-ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/carla/Town10HD.osm
+ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/ATC_demo_2024-05-24.osm /data/maps/default/ATC.osm
+ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/default/CampusVaalsHalifax.osm
+ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/default/Town10HD.osm
