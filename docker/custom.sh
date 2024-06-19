c230830f6a10ea49c33f953d20b8e8251ff30382
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-06-19 13:39:47 +0200

    add custom.sh

---
+++ docker/custom.sh
@@ -0,0 +1,6 @@
+ln -s /docker-ros/additional-files /data/maps
+ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/germany-aldenhoven-atc.osm /data/maps/ATC.osm
+ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/CampusVaalsHalifax.osm
+ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
+
+ /data/launch
