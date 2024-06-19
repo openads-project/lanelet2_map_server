9a23c2710e249c0afe63c78333b09e046156b21d
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-06-19 14:07:12 +0200

    fix symbolic links in custom.sh

--- docker/custom.sh
+++ docker/custom.sh
@@ -2,7 +2,7 @@
 mkdir -p /data/maps
 ln -s /docker-ros/additional-files /data/maps
 
-# add symbolic links to provide default maps for typical locations
-ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/germany-aldenhoven-atc.osm /data/maps/ATC.osm
-ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/CampusVaalsHalifax.osm
+# add symbolic links to provide default maps for the carla_its_adapter
+ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/ATC_demo_2024-05-24.osm /data/maps/ATC.osm
+ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/CampusVaalsHalifax.osm /data/maps/CampusVaalsHalifax.osm
 ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
