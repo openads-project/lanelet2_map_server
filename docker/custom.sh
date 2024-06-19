95f29fb1050dc21600a49a12694f70b738f82853
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-06-19 13:59:00 +0200

    bugfix in custom.sh

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,7 +1,8 @@
+# create symbolic links to provide maps in /data/maps
+mkdir -p /data/maps
 ln -s /docker-ros/additional-files /data/maps
 
 # add symbolic links to provide default maps for typical locations
-mkdir -p /data/maps
-ln -L /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/germany-aldenhoven-atc.osm /data/maps/ATC.osm
-ln -L /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/CampusVaalsHalifax.osm
-ln -L /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
+ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/germany-aldenhoven-atc.osm /data/maps/ATC.osm
+ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/CampusVaalsHalifax.osm
+ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
