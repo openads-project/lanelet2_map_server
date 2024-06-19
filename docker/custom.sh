34277627762556c5b97a256e413d86c1c8d6cb89
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-06-19 13:55:17 +0200

    use logical links

--- docker/custom.sh
+++ docker/custom.sh
@@ -2,6 +2,6 @@ ln -s /docker-ros/additional-files /data/maps
 
 # add symbolic links to provide default maps for typical locations
 mkdir -p /data/maps
-ln -sf /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/germany-aldenhoven-atc.osm /data/maps/ATC.osm
-ln -sf /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/CampusVaalsHalifax.osm
-ln -sf /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
+ln -L /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/germany-aldenhoven-atc.osm /data/maps/ATC.osm
+ln -L /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/CampusVaalsHalifax.osm
+ln -L /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
