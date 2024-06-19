4fe8686e00477ece0c5204bc5b5a4ed276245f06
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-06-19 13:48:23 +0200

    create /data/maps folder

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,6 +1,7 @@
 ln -s /docker-ros/additional-files /data/maps
 
 # add symbolic links to provide default maps for typical locations
-ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/germany-aldenhoven-atc.osm /data/maps/ATC.osm
-ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/CampusVaalsHalifax.osm
-ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
+mkdir -p /data/maps
+ln -sf /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/germany-aldenhoven-atc.osm /data/maps/ATC.osm
+ln -sf /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/CampusVaalsHalifax.osm
+ln -sf /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
