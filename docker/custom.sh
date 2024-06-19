54867b0879f4d77f32bd4e19de1f087efcadb03c
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-06-19 12:43:34 +0000

    Rename symbolic link for locations folder

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,6 +1,6 @@
 # create symbolic links to provide maps in /data/maps
 mkdir -p /data/maps/default
-ln -s /docker-ros/additional-files /data/maps
+ln -s /docker-ros/additional-files /data/maps/locations
 
 # add symbolic links to provide default maps for the carla_its_adapter
 ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/ATC_demo_2024-05-24.osm /data/maps/default/ATC.osm
