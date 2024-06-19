86b6fc749658e351b732cd9d1c2fab9e6272f75a
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-06-19 14:20:48 +0200

    add dedicated location for carla symlinks

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,8 +1,8 @@
 # create symbolic links to provide maps in /data/maps
-mkdir -p /data/maps
+mkdir -p /data/maps/carla
 ln -s /docker-ros/additional-files /data/maps
 
 # add symbolic links to provide default maps for the carla_its_adapter
-ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/ATC_demo_2024-05-24.osm /data/maps/ATC.osm
-ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/CampusVaalsHalifax.osm /data/maps/CampusVaalsHalifax.osm
-ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/Town10HD.osm
+ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/ATC_demo_2024-05-24.osm /data/maps/carla/ATC.osm
+ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack-demo/TesttrackDemo.osm /data/maps/carla/CampusVaalsHalifax.osm
+ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/carla/Town10HD.osm
