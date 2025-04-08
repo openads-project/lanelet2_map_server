6d75f343b6a1ac2c9d00b6b7c953c1ee48860f45
Christian Geller <christian.geller@ika.rwth-aachen.de>
2025-04-08 06:31:02 +0000

    remove carla specific symbolic links

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,10 +1,2 @@
-# add symbolic links to provide default maps for the carla_its_adapter
-mkdir -p /data/maps/carla
-ln -s /docker-ros/additional-files/synthetic-carla/lanelet2/town10hd/Town10HD.osm /data/maps/carla/Town10HD.osm
-ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/ATC_demo_2024-05-24.osm /data/maps/carla/ATC.osm
-ln -s /docker-ros/additional-files/germany-aldenhoven-atc/lanelet2/unicaragil-atlatec/ATC_demo_2024-05-24.osm /data/maps/carla/aldenhoven.osm
-ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack/ika-testtrack-autoshuttle.osm /data/maps/carla/CampusVaalsHalifax.osm
-ln -s /docker-ros/additional-files/germany-aachen-campusmelaten/lanelet2/ika-testtrack/ika-testtrack-autoshuttle.osm /data/maps/carla/ika-test-track.osm
-
 # create symbolic links to provide all maps in /data/maps
 ln -s /docker-ros/additional-files /data/maps/locations
