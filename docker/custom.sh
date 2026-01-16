3dcc3d7628391a4a8106077dd3af2d3284eca141
Christian Geller <christian.geller@ika.rwth-aachen.de>
2026-01-16 14:49:38 +0100

    update path

--- docker/custom.sh
+++ docker/custom.sh
@@ -3,7 +3,7 @@ mkdir -p /data/lanelet2-maps
 
 curl --fail --location \
     --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-    "https://gitlab.ika.rwth-aachen.de/api/v4/projects/2173/packages/generic/lanelet2-maps/update/structure/lanelet2-maps.tar.gz" \
+    "https://gitlab.ika.rwth-aachen.de/api/v4/projects/2173/packages/generic/lanelet2-maps/update-structure/lanelet2-maps.tar.gz" \
     --output /data/lanelet2-maps/package.tar.gz
     
 tar -xzf /data/lanelet2-maps/package.tar.gz -C /data/lanelet2-maps
