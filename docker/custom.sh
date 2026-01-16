e35eab3be6864a99e4d6ab4a47b4ca0f8b0c9561
Christian Geller <christian.geller@ika.rwth-aachen.de>
2026-01-16 15:30:14 +0100

    use wget

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,9 +1,6 @@
 # download lanelet2 maps from locations package registry
 mkdir -p /data/lanelet2-maps
 
-curl --fail --location \
-    --header "JOB-TOKEN:$GIT_HTTPS_PASSWORD" \
-    "https://gitlab.ika.rwth-aachen.de/api/v4/projects/2173/packages/generic/lanelet2-maps/update-structure/lanelet2-maps.tar.gz" \
-    --output /data/lanelet2-maps/package.tar.gz
+wget -O /data/lanelet2-maps/package.tar.gz https://gitlab.ika.rwth-aachen.de/api/v4/projects/2173/packages/generic/lanelet2-maps/update-structure/lanelet2-maps.tar.gz?access_token=$GIT_HTTPS_PASSWORD
     
 tar -xzf /data/lanelet2-maps/package.tar.gz -C /data/lanelet2-maps
